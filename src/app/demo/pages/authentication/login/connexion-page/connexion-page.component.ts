c13d37a6132464160b02736b9ab140be46a9145e
apple <xpertproinformatique@gmail.com>
2025-02-17 10:29:05 -0800

    connexion component and users services connexion

---
+++ src/app/demo/pages/authentication/login/connexion-page/connexion-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion-page',
+  imports: [],
+  templateUrl: './connexion-page.component.html',
+  styleUrl: './connexion-page.component.scss'
+})
+export class ConnexionPageComponent {
+
+}
