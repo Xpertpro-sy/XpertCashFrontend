a467279a27e1d9fae98dbef99d26bcff206f7dea
apple <xpertproinformatique@gmail.com>
2025-02-17 13:43:38 -0800

    last updat

--- src/app/admin-page/SERVICES/users.service.ts
+++ src/app/admin-page/SERVICES/users.service.ts
@@ -8,15 +8,18 @@ import { Observable } from 'rxjs';
 })
 export class UsersService {
   private apiUrl: string = "http://localhost:8080/api/auth";
+  
 
   constructor(private http: HttpClient) {}
 
-  /**
-   * Envoie les données d'inscription à l'API et retourne une réponse typée
-   * @param user - Données de l'utilisateur à inscrire
-   * @returns Observable avec la réponse du backend sous forme d'objet JSON
-   */
-  registerUser(user: Users): Observable<{ message?: string; error?: string }> {
-    return this.http.post<{ message?: string; error?: string }>(`${this.apiUrl}/register`, user);
+  //Inscription
+    registerUser(user: Users): Observable<{ message?: string; error?: string }> {
+      return this.http.post<{ message?: string; error?: string }>(`${this.apiUrl}/register`, user);
+    }
+
+  // Connexion
+    loginUser(credentials: { email: string; password: string }): Observable<{ message?: string; token?: string; error?: string }> {
+      return this.http.post<{ message?: string; token?: string; error?: string }>(`${this.apiUrl}/login`, credentials);
   }
+  
 }
